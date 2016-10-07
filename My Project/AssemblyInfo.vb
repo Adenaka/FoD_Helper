51c45304e2a484bd078fb351515c9bebf8112be0
Adenaka <Permakov2@gmail.com>
2016-10-07 10:39:16 +0300

    1.1 Guild (folders)

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Adenaka's Gold Helper")>
+<Assembly: AssemblyDescription("Something like automatic calc for Flight of Dragons")>
+<Assembly: AssemblyCompany("Adenaka")>
+<Assembly: AssemblyProduct("Gold Helper")>
+<Assembly: AssemblyCopyright("Adenaka (r)  2016")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("2d319766-8600-40ec-9f25-ffa548fcbd26")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер построения
+'      Редакция
+'
+' Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
